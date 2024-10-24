08e196bad1135fa46475dfbad336149421d40630
ethanwong2008 <ewong3883@gmail.com>
2024-10-24 14:24:02 -0700

    pls change

--- assets/js/rpg2x/GameLevelWater.js
+++ assets/js/rpg2x/GameLevelWater.js
@@ -26,16 +26,16 @@ class GameLevelWater {
     };
 
     // Player 1 sprite data (turtle)
-    const TURTLE_SCALE_FACTOR = 10;
-    const sprite_src_turtle = path + "/images/rpg/turtle.png";
-    const sprite_data_turtle = {
-        name: 'turtle',
-        src: sprite_src_turtle,
-        SCALE_FACTOR: TURTLE_SCALE_FACTOR,
+    const sprite_SCALE_FACTOR = 10;
+    const sprite_src_sprite = path + "/images/rpg/sprite.png";
+    const sprite_data_sprite = {
+        name: 'sprite',
+        src: sprite_src_sprite,
+        SCALE_FACTOR: SPRITE_SCALE_FACTOR,
         STEP_FACTOR: 1000,
         ANIMATION_RATE: 50,
-        INIT_POSITION: { x: 200, y: height - (height/TURTLE_SCALE_FACTOR) }, 
-        pixels: {height: 280, width: 256},
+        INIT_POSITION: { x: 0, y: height - (height/SPRITE_SCALE_FACTOR) }, 
+        pixels: {height: 72, width: 48},
         orientation: {rows: 4, columns: 3 },
         down: {row: 0, start: 0, columns: 3 },
         left: {row: 1, start: 0, columns: 3 },
