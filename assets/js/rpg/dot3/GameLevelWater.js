21a40d7c44136a807fb3da2cebdc33bf10bade7c
ethanwong2008 <ewong3883@gmail.com>
2024-11-04 16:38:08 -0800

    Update GameLevelWater.js

--- assets/js/rpg/dot3/GameLevelWater.js
+++ assets/js/rpg/dot3/GameLevelWater.js
@@ -1,108 +1,88 @@
+// To build GameLevels, each contains GameObjects from below imports
 import GameEnv from './GameEnv.js';
 import Background from './Background.js';
 import PlayerOne from './PlayerOne.js';
 import PlayerTwo from './PlayerTwo.js';
+import NPC from './NPC.js';
+//import Goomba from './EnemyGoomba.js';
+//import Coin from './Coin.js';
+
 
 class GameLevelWater {
   constructor(path) {
     const header = document.querySelector('header');
     const footer = document.querySelector('footer');
 
+    // Values dependent on GameEnv.create()
     let width = GameEnv.innerWidth;
     let height = GameEnv.innerHeight;
 
     // Background data
     const image_src_water = path + "/images/rpg/map.jpg";
     const image_data_water = {
-      name: 'water',
-      src: image_src_water,
-      pixels: { height: 580, width: 1038 }
+        name: 'water',
+        src: image_src_water,
+        pixels: {height: 580, width: 1038}
     };
 
-    // Player 1 sprite data (Luigi)
+    // Player 1 sprite data (turtle)
     const TURTLE_SCALE_FACTOR = 10;
     const sprite_src_turtle = path + "/images/rpg/luigi.png";
     const sprite_data_turtle = {
-      name: 'turtle',
-      src: sprite_src_turtle,
-      SCALE_FACTOR: TURTLE_SCALE_FACTOR,
-      STEP_FACTOR: 1000,
-      ANIMATION_RATE: 50,
-      INIT_POSITION: { x: 0, y: height - (height / TURTLE_SCALE_FACTOR) },
-      pixels: { height: 72, width: 48 },
-      orientation: { rows: 4, columns: 3 },
-      down: { row: 0, start: 0, columns: 3 },
-      left: { row: 1, start: 0, columns: 3 },
-      right: { row: 2, start: 0, columns: 3 },
-      up: { row: 3, start: 0, columns: 3 },
+        name: 'turtle',
+        src: sprite_src_turtle,
+        SCALE_FACTOR: TURTLE_SCALE_FACTOR,
+        STEP_FACTOR: 1000,
+        ANIMATION_RATE: 50,
+        INIT_POSITION: { x: 0, y: height - (height/TURTLE_SCALE_FACTOR) }, 
+        pixels: {height: 72, width: 48},
+        orientation: {rows: 4, columns: 3 },
+        down: {row: 0, start: 0, columns: 3 },
+        left: {row: 1, start: 0, columns: 3 },
+        right: {row: 2, start: 0, columns: 3 },
+        up: {row: 3, start: 0, columns: 3 },
     };
 
     // Player 2 sprite data (fish)
     const sprite_src_fish = path + "/images/rpg/fishies.png";
     const sprite_data_fish = {
-      name: 'fish',
-      src: sprite_src_fish,
-      SCALE_FACTOR: 16,
-      STEP_FACTOR: 400,
-      ANIMATION_RATE: 50,
-      pixels: { height: 256, width: 384 },
-      INIT_POSITION: { x: 0, y: 0 },
-      orientation: { rows: 8, columns: 12 },
-      down: { row: 0, start: 0, columns: 3 },
-      left: { row: 1, start: 0, columns: 3 },
-      right: { row: 2, start: 0, columns: 3 },
-      up: { row: 3, start: 0, columns: 3 },
+        name: 'fish',
+        src: sprite_src_fish,
+        SCALE_FACTOR: 16,
+        STEP_FACTOR: 400,
+        ANIMATION_RATE: 50,
+        pixels: {height: 256, width: 384},
+        INIT_POSITION: { x: 0, y: 0 },
+        orientation: {rows: 8, columns: 12 },
+        down: {row: 0, start: 0, columns: 3 },  // 1st row
+        left: {row: 1, start: 0, columns: 3 },  // 2nd row
+        right: {row: 2, start: 0, columns: 3 }, // 3rd row
+        up: {row: 3, start: 0, columns: 3 },    // 4th row
     };
 
-    // Instantiate game objects
+    // NPC sprite data (frog)
+    const sprite_src_frog = path + "/images/rpg/fishies.png";
+    const sprite_data_frog = {
+        name: 'npc',
+        src: sprite_src_frog,
+        SCALE_FACTOR: 16,  // Adjust this based on your scaling needs
+        ANIMATION_RATE: 50,
+        pixels: {height: 256, width: 384},
+        INIT_POSITION: { x: (width / 2), y: (height / 2)},
+        orientation: {rows: 8, columns: 12 },
+        down: {row: 0, start: 9, columns: 3 },  // This is the stationary npc, down is default 
+    };
+
+    // List of objects defnitions for this level
     this.objects = [
-      new Background(image_data_water),
-      new PlayerOne(sprite_data_turtle),
-      new PlayerTwo(sprite_data_fish)
+      { class: Background, data: image_data_water },
+      { class: PlayerOne, data: sprite_data_turtle },
+      { class: PlayerTwo, data: sprite_data_fish },
+      { class: NPC, data: sprite_data_frog }
     ];
-
-    // Start the game loop
-    this.gameLoop();
   }
 
-  // Collision detection function for two objects
-  isColliding(obj1, obj2) {
-    const pos1 = obj1.getPosition();
-    const size1 = obj1.getSize();
-    const pos2 = obj2.getPosition();
-    const size2 = obj2.getSize();
-
-    return (
-      pos1.x < pos2.x + size2.width &&
-      pos1.x + size1.width > pos2.x &&
-      pos1.y < pos2.y + size2.height &&
-      pos1.y + size1.height > pos2.y
-    );
-  }
-
-  // Game loop function
-  gameLoop() {
-    const playerOne = this.objects[1]; // Luigi (PlayerOne)
-    const playerTwo = this.objects[2]; // Fish (PlayerTwo)
-
-    // Check for collision between Luigi and Fish
-    if (this.isColliding(playerOne, playerTwo)) {
-      this.handleCollision(playerOne, playerTwo);
-    }
-
-    // Update and redraw all objects
-    this.objects.forEach(obj => obj.update());
-
-    // Continue the game loop
-    requestAnimationFrame(() => this.gameLoop());
-  }
-
-  // Handle collision between Luigi and Fish
-  handleCollision(obj1, obj2) {
-    console.log(`Collision detected between ${obj1.data.name} and ${obj2.data.name}`);
-    // You can add additional collision handling code here, such as stopping movement,
-    // triggering animations, reducing health, etc.
-  }
 }
 
 export default GameLevelWater;
+
